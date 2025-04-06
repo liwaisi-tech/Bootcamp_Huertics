bf7cee9be7e0ccdae5f88a24c7f61d5dcbab0473
Ladybataca <btatacc@gmail.com>
2025-04-06 16:22:10 -0500

    código base para sesion 2

--- main/bootcamp_huertics.c
+++ main/bootcamp_huertics.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include "moisture_sensor.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
 
+static const char *TAG = "Capacitive-sensor";
+#define CHANNEL1 ADC_CHANNEL_5 //Pin GPIO33 (D33). Asegurate que sea el pin que uses en la conexión fisica.
 void app_main(void)
 {
-
+    int humedad = 0;
+    moisture_sensor_config_t cfg = SENSOR_DEFAULT_CONFIG;
+    cfg.channel = CHANNEL1;
+    cfg.sensor_type = TYPE_CAP;
+    moisture_sensor_init(&cfg); 
+    
 }
